fffa4ef1f518961830c390e42bc39fcd74ceafb0
hemanivetha email = hemanivetha25@gmail.com <User1@LP-5CD9296CML.HCLT.CORP.HCL.IN>
2020-01-22 11:04:00 +0530

    moneydeposit service

---
+++ moneydeposite-service/moneydeposite-service/src/main/java/com/uob/customer/account/moneydepositeservice/MoneydepositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uob.customer.account.moneydepositeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneydepositeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneydepositeServiceApplication.class, args);
+	}
+
+}
---
+++ moneydeposite-service/moneydeposite-service/src/test/java/com/uob/customer/account/moneydepositeservice/MoneydepositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uob.customer.account.moneydepositeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneydepositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
