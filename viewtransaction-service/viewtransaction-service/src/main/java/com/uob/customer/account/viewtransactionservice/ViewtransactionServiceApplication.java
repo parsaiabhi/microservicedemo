60e2c5bb4dcfd1edf58e4a27d421d052591b9ad1
hemanivetha email = hemanivetha25@gmail.com <User1@LP-5CD9296CML.HCLT.CORP.HCL.IN>
2020-01-22 11:05:35 +0530

    viewtransaction service

---
+++ viewtransaction-service/viewtransaction-service/src/main/java/com/uob/customer/account/viewtransactionservice/ViewtransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uob.customer.account.viewtransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewtransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewtransactionServiceApplication.class, args);
+	}
+
+}
